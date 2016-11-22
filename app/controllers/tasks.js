62cf20e4a8af7ff2726e88b2dd2ae1c327b9addb
ayman khalel <evoix.a@gmail.com>
2016-11-22 18:25:12 +0200

    fourth

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -2,21 +2,32 @@
 	var task = angular.module("app");
 	task.controller("tasks",tasks);
 	
-	function tasks($scope){
+	function tasks($scope, $timeout){
+		$scope.Tasks=[];
+		$scope.Tasks=localStorage.getItem('Tasks');
+		$scope.Tasks=JSON.parse($scope.Tasks);
+		if($scope.Tasks===null)
+		{
+			$scope.Tasks=[];
+		}
+		
+		
+		console.log("Tasks:  " +$scope.Tasks)
 		$scope.taskH="";
 		$scope.taskM="";
 		$scope.taskD="";
 		$scope.taskMO="";
 		$scope.taskY="";
-		$scope.Tasks=[];
 		$scope.taskContent="";
-		
+		$scope.TL=[]
 		$scope.getTask=function(){
 			$scope.date=[$scope.taskH.toString() + ":" + $scope.taskM.toString(),$scope.taskD.toString()+"-"+$scope.taskMO.toString()+"-"+$scope.taskY.toString()]
 			$scope.Task=[$scope.date,$scope.taskContent]
 			
 			$scope.Tasks.unshift($scope.Task);
-			$scope.TL=$scope.Tasks.length;
+			$scope.TL.unshift($scope.Tasks.length);
+			localStorage.setItem('Tasks', JSON.stringify($scope.Tasks));
+
 		};
 		
 	}
